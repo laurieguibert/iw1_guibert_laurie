fb64e910f664b8d4fdd6c3f96d71360d3cf35d51
laurieguibert <l.guibert@hotmail.fr>
2017-10-11 20:41:38 +0200

    Bot réservation hôtel
    Utilisation des libraries + convert date

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
- * Created by lguib on 12/09/2017.
+ * Created by lguib on 11/10/2017.
  */
 var restify = require('restify');
 var botbuilder = require ('botbuilder');
@@ -21,55 +21,74 @@ var connector = new botbuilder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Reply by echoing
-var bot = new botbuilder.UniversalBot(connector, function(session){
-    //session.send('you have tapped : ${session.message.text} | [length : ${session.message.text.length}');
-    session.send(`Vous avez écrit : %s | [longueur du texte : %s]`, session.message.text, session.message.text.length);
-    /*session.send(JSON.stringify(session.dialogData));
-    session.send(JSON.stringify(session.sessionState));
-    session.send(JSON.stringify(session.conversationData));
-    session.send(JSON.stringify(session.userData));*/
+var bot = new botbuilder.UniversalBot(connector, [
+    function(session){
+        session.beginDialog('getUser', session.userData.profile);
+    },
+    function(session, results){
+        session.userData.profile = results.response;
+        session.send(`Procédons maintenant à la réservation ${session.userData.profile.firstname} ${session.userData.profile.name} `);
+        session.beginDialog('reservation:hotel');
+    },
+    function(session, results){
+        session.userData.reservation = results.response;
+        var date = new Date(session.userData.reservation.reservationDate).toLocaleString("fr-FR");
+        session.send(`Votre réservation est confirmée pour le ${date} pour ${session.userData.reservation.nbNight} nuit(s) à ${session.userData.reservation.destination.name} au nom de ${session.userData.profile.firstname} ${session.userData.profile.name} (${session.userData.profile.age} ans). <br/> Un email de confirmation vous sera envoyé à l'adresse suivante : ${session.userData.profile.email}`);
+        session.endConversation();
+    }
+]);
 
-    bot.on('typing', function(){
-        session.send("Utilisateur en train decrire");
-    });
-
-    bot.on('conversationUpdate', function(message) {
-        if(message.membersAdded && message.membersAdded.length > 0){
-            var membersAdded = message.membersAdded
-            .map(function(x){
-                var isSelf = x.id === message.address.bot.id;
-                return (isSelf ? message.address.bot.name : x.name) || ' ' + '(Id = ' + x.id + ')'
-            })
-            .join(', ');
-            bot.send(new botbuilder.Message()
-            .address(message.address)
-            .text('Bienvenue ' + membersAdded));
+bot.dialog('getUser', [
+    function (session, args, next){
+        session.dialogData.profile = args || {};
+        if(!session.dialogData.profile.name){
+            botbuilder.Prompts.text(session,"Bonjour, quel est votre nom ?");
+        } else {
+            next();
+        }
+    }, 
+    function (session, results, next){
+        if(results.response){
+            session.dialogData.profile.name = results.response;
         }
 
-        if(message.membersRemoved && message.membersRemoved.length > 0){
-            var membersRemoved = message.membersRemoved
-            .map(function(x){
-                var isSelf = x.id === message.address.bot.id;
-                return (isSelf ? message.address.bot.id : x.id) || ' ' + '(Id = ' + x.id + ')'
-            })
-            .join(', ');
-            bot.send(new botbuilder.Message()
-            .address(message.address)
-            .text('Au revoir ' + membersRemoved));
+        if(!session.dialogData.profile.firstname){
+            botbuilder.Prompts.text(session,"Et quel est votre prénom ?");
+        } else {
+            next();
         }
-    });
-
-    bot.on('contactRelationUpdate', function(message){
-        if(message.action === 'add'){
-            var response = 'Bienvenue ';
+    },
+    function (session, results, next){
+        if(results.response){
+            session.dialogData.profile.firstname = results.response;
         }
 
-        if(message.action === 'remove'){
-            var response = 'Au revoir ';
+        if(!session.dialogData.profile.age){
+            botbuilder.Prompts.number(session,"Et quel est votre âge ?");
+        } else {
+            next();
+        }
+    },
+    function (session, results, next){
+        if(results.response){
+            session.dialogData.profile.age = results.response;
         }
 
-        bot.send(new botbuilder.Message()
-        .address(message.address)
-        .text(response + message.address.bot.name + '-' + message.address.bot.id));
-    })
-});
+        if(!session.dialogData.profile.email){
+            botbuilder.Prompts.text(session,"Quel est votre adresse email ?");
+        } else {
+            next();
+        }
+    },
+    function (session, results){
+        if(results.response){
+            session.dialogData.profile.email = results.response;
+        }
+
+        session.endDialogWithResult({ response: session.dialogData.profile });
+
+        //session.endDialog(`Bonjour ${session.dialogData.profile.firstname} ${session.dialogData.profile.name}`);
+    }
+]);
+
+bot.library(require('./dialogs/hotels.js'));
